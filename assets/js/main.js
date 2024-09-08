a556198a8b22ea884ec3d7968132a3c865f4462a
robertopar98 <148049283+robertopar98@users.noreply.github.com>
2024-09-08 21:27:06 +0200

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,55 @@
+
+function toggleDetails(iconElement) {
+    const details = iconElement.nextElementSibling;
+    if (details.style.display === "block") {
+        details.style.display = "none";
+    } else {
+        details.style.display = "block";
+    }
+};
+
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const darkModeToggle = document.querySelector('.dark-mode-toggle-img');
+    const lightModeImage = 'assets/images/day-mode-icon.png';
+    const darkModeImage = 'assets/images/night-mode-icon.png';
+
+    // Check if there's a saved theme in localStorage
+    const currentTheme = localStorage.getItem('theme');
+    
+    // Apply the saved theme
+    if (currentTheme) {
+        document.documentElement.setAttribute('data-theme', currentTheme);
+        
+        // Adjust the button label based on the theme
+        if (currentTheme === 'dark') {
+            darkModeToggle.textContent = 'Light Mode';
+            darkModeToggle.src = darkModeImage;
+        } else {
+            darkModeToggle.src = lightModeImage;
+        }
+    } else {
+        darkModeToggle.src = lightModeImage;
+    }
+
+    // Add event listener to toggle button
+    darkModeToggle.addEventListener('click', function(event) {
+        event.preventDefault();
+        
+        // Toggle dark mode
+        if (document.documentElement.getAttribute('data-theme') === 'dark') {
+            document.documentElement.removeAttribute('data-theme');
+            localStorage.setItem('theme', 'light'); // Save preference in localStorage
+            darkModeToggle.textContent = 'Dark Mode'; // Update the button label
+            darkModeToggle.src = lightModeImage;
+        } else {
+            document.documentElement.setAttribute('data-theme', 'dark');
+            localStorage.setItem('theme', 'dark'); // Save preference in localStorage
+            darkModeToggle.textContent = 'Light Mode'; // Update the button label
+            darkModeToggle.src = darkModeImage;
+
+        }
+    });
+});
