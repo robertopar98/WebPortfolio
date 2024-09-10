21abafec4cdf1d5b4631cd488e85702eb12ae682
robertopar98 <148049283+robertopar98@users.noreply.github.com>
2024-09-10 21:26:57 +0200

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -53,3 +53,13 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 });
+
+
+function toggleMenu() {
+    var navLinks = document.getElementById("navLinks");
+    if (navLinks.classList.contains("active")) {
+        navLinks.classList.remove("active");
+    } else {
+        navLinks.classList.add("active");
+    }
+}
